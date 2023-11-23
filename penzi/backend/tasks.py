8f5b86be801a07d1de7c6a9944c8d8a6609f455a
MarkMoki <tygameme391@gmail.com>
2023-11-23 18:06:00 +0300

    cleaning a bit

--- penzi/backend/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-from .helpers import send_response_function  # Import your function for sending responses
-
-@shared_task
-def send_multiple_responses(response1, response2):
-    # Call your function to send responses asynchronously
-    send_response_function(response1)
-    send_response_function(response2)
